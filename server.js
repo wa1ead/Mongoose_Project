18c634bb2a654d69ff1cea180d7c24608aae62e1
wa1ead <waliddraa2@gmail.com>
2024-10-29 09:53:34 +0100

    creating person model record

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
+const Person = require("./Person");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const person = new Person({ name: "Walid", age: 22, favoriteFoods: "fish" });
+
+person
+  .save()
+  .then(() => {
+    console.log(person);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
