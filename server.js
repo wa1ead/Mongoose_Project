7729ad2a93bc157c2619896dd04c4ff830139234
wa1ead <waliddraa2@gmail.com>
2024-10-29 10:12:06 +0100

    creating several people records

--- server.js
+++ server.js
@@ -11,13 +11,43 @@ mongoose
     console.error(err);
   });
 
-const person = new Person({ name: "Walid", age: 22, favoriteFoods: "fish" });
+// const person = new Person({ name: "Walid", age: 22, favoriteFoods: "fish" });
 
-person
-  .save()
-  .then(() => {
-    console.log(person);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// person
+//   .save()
+//   .then(() => {
+//     console.log(person);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+async function arrayOfPeople() {
+  const people = await Person.create([
+    {
+      name: "Yassine",
+      age: 20,
+      favoriteFoods: "pastilla",
+    },
+    {
+      name: "Moussa",
+      age: 33,
+      favoriteFoods: "tajine",
+    },
+    {
+      name: "Lmorphine",
+      age: 42,
+      favoriteFoods: "pastech",
+    },
+    {
+      name: "inkonnu",
+      age: 28,
+      favoriteFoods: "baghrir",
+    },
+    { name: "EGT", age: 26, favoriteFoods: "jackda" },
+  ]);
+
+  console.log(people);
+}
+
+arrayOfPeople();
